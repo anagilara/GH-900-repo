65e13e48e79f4e6fefa99ec00839a6225a3dc6a1
anagilara <110118950+anagilara@users.noreply.github.com>
2025-12-08 17:45:27 -0600

    Prbranch Squash (#25)
    * modified 2 files second commit
    
    * second 3 files

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 //test 2
 //test 3
 //test 2 PR
+//squash test PR 1
+//squash test PR 4
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
